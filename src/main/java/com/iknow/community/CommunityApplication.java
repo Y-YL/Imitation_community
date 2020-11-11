e0620b1e5a1055f0122b415cc759077ccc0a9916
qifenhaoyue <1209796758@qq.com>
2020-11-11 22:29:17 +0800

    启动类
    Signed-off-by: qifenhaoyue <1209796758@qq.com>

--- src/main/java/com/iknow/community/CommunityApplication.java
+++ src/main/java/com/iknow/community/CommunityApplication.java
@@ -3,8 +3,23 @@ package com.iknow.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CommunityApplication {
+
+	/**
+	 *
+	 */
+	@PostConstruct
+	public void init(){
+		// 解决Netty启动冲突的问题
+		// 原因：availableProcessors  redis和elasticsearch都会依赖，但是当redis已经启动了一个
+		// availableProcessors后，elasticsearch会检测，发现存在就会报错，所以设置下面属性，可以避免报错
+		System.setProperty("es.set.netty.runtime.available.processors","false");
+
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
