4b5f90fd127959d0b84f7bdd9f29f8840fcae454
qifenhaoyue <1209796758@qq.com>
2020-11-20 21:06:37 +0800

    ThreadLocal配置类
    Signed-off-by: qifenhaoyue <1209796758@qq.com>

---
+++ src/main/java/com/iknow/community/config/ThreadPoolConfig.java
@@ -0,0 +1,11 @@
+package com.iknow.community.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@EnableAsync
+public class ThreadPoolConfig {
+}
