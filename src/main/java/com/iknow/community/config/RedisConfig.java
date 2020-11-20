e4e215891bafe95902b32e9376578ea174f34e46
qifenhaoyue <1209796758@qq.com>
2020-11-20 21:06:10 +0800

    redis配置类
    Signed-off-by: qifenhaoyue <1209796758@qq.com>

---
+++ src/main/java/com/iknow/community/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.iknow.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String ,Object> redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String,Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        // 设置key的序列化方式
+        template.setKeySerializer(RedisSerializer.string());
+        // 设置value的序列化方式
+        template.setValueSerializer(RedisSerializer.json());
+        // 设置hash的key的序列化方式
+        template.setHashKeySerializer(RedisSerializer.string());
+        // 设置hash的value的序列化方式
+        template.setHashKeySerializer(RedisSerializer.json());
+        // 使设置生效
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
