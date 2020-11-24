95f2a5b3c7d4e25071c3aed46c296c9746eeb2b4
qifenhaoyue <1209796758@qq.com>
2020-11-24 20:20:29 +0800

    方法拦截
    Signed-off-by: qifenhaoyue <1209796758@qq.com>

---
+++ src/main/java/com/iknow/community/Controller/interceptor/LoginRequiredInterceptor.java
@@ -0,0 +1,35 @@
+package com.iknow.community.Controller.interceptor;
+
+import com.iknow.community.annotation.LoginRequired;
+import com.iknow.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+@Component
+public class LoginRequiredInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    HostHolder hostHolder;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        // 拦截方法，需要先判断，拦截的是否为方法
+        if (handler instanceof HandlerMethod){
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            Method method = handlerMethod.getMethod();
+            LoginRequired loginRequired = method.getAnnotation(LoginRequired.class);
+            if (loginRequired!=null && hostHolder.getUser()==null){
+                response.sendRedirect(request.getContextPath()+"/login");
+                return false;
+            }
+        }
+        return true;
+    }
+}
