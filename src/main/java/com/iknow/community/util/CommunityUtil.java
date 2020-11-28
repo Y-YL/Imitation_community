3e5ce694198d08b1a0b87a7cb26039ee4f43ef71
qifenhaoyue <1209796758@qq.com>
2020-11-28 19:03:01 +0800

    CommunityUtil
    Signed-off-by: qifenhaoyue <1209796758@qq.com>

---
+++ src/main/java/com/iknow/community/util/CommunityUtil.java
@@ -0,0 +1,54 @@
+package com.iknow.community.util;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.DigestUtils;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class CommunityUtil {
+
+    // 生成随机字符串
+    public static String generateUUID(){
+        return UUID.randomUUID().toString().replaceAll("-","");
+    }
+
+    // MD5加密
+    // 对密码进行加密
+    // key - 加盐后
+    public static String md5(String key){
+        if (StringUtils.isBlank(key)){
+            return null;
+        }
+        return DigestUtils.md5DigestAsHex(key.getBytes());
+    }
+
+    public static String getJSONString(int code,String msg,Map<String,Object> map){
+        JSONObject json = new JSONObject();
+        json.put("code",code);
+        json.put("msg",msg);
+        if (map!=null){
+            for (String key:map.keySet()){
+                json.put(key,map.get(key));
+            }
+        }
+        return json.toJSONString();
+    }
+
+    public static String getJSONString(int code,String msg){
+        return getJSONString(code,msg,null);
+    }
+
+    public static String getJSONString(int code){
+        return getJSONString(code,null,null);
+    }
+
+//    public static void main(String[] args){
+//        Map<String,Object> map = new HashMap<>();
+//        map.put("name","zhangsan");
+//        map.put("age",25);
+//        System.out.println(getJSONString(0,"ok",map));
+//
+//    }
+}
