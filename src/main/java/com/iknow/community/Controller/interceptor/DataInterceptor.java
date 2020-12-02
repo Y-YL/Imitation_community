289a8b73b3997e5b763aaa1e485463f615e48fa4
qifenhaoyue <1209796758@qq.com>
2020-12-02 14:36:50 +0800

    DAU UV 统计
    Signed-off-by: qifenhaoyue <1209796758@qq.com>

---
+++ src/main/java/com/iknow/community/Controller/interceptor/DataInterceptor.java
@@ -0,0 +1,35 @@
+package com.iknow.community.Controller.interceptor;
+
+import com.iknow.community.bean.User;
+import com.iknow.community.service.DataService;
+import com.iknow.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class DataInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private DataService dataService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 统计UV
+        String ip = request.getRemoteHost();
+        dataService.recordUV(ip);
+
+        // 统计DAU
+        User user = hostHolder.getUser();
+        if (user != null){
+            dataService.recordDAU(user.getId());
+        }
+        return true;
+    }
+}
